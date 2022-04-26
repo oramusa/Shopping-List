3f48aed87f34189f0dd45c3e33b707158467178b
Alpay Tonga <alpaytonga@alpays-imac.attlocal.net>
2022-04-26 12:20:43 -0400

    Deployed website

---
+++ src/ItemList.js
@@ -0,0 +1,18 @@
+import LineItem from './LineItem';
+
+const ItemList = ({ items, handleCheck, handleDelete }) => {
+    return (
+        <ul className='ul'>
+            {items.map((item) => (
+                <LineItem
+                    key={item.id}
+                    item={item}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default ItemList
